7f4d5790f864a98d40e25a61727823b0dd844b79
Devill0104 <adityamaurya533@gmail.com>
2024-04-16 03:10:51 +0530

    data initialized

---
+++ init/index.js
@@ -0,0 +1,20 @@
+const mongoose=require("mongoose");
+const mongo_url="mongodb://127.0.0.1:27017/wanderlust";
+const Listing = require("../models/listing.js");
+const initData= require("./data.js");
+
+//connecting to the database 
+main().then(()=>{
+    console.log("connected to db");
+}).catch((err)=> console.log(err));
+async function main(){
+    await mongoose.connect(mongo_url);
+}
+
+const initDB = async ()=>{
+    await Listing.deleteMany({});
+    await Listing.insertMany(initData.data);
+    console.log("data inserted successfully");
+}
+
+initDB();
