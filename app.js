7c9226cd6cd36b3aa4acaa292a6401d0417f0035
Devill0104 <adityamaurya533@gmail.com>
2024-04-22 15:57:07 +0530

    css basic and bpc done

--- app.js
+++ app.js
@@ -5,10 +5,14 @@ const mongoose=require("mongoose");
 const mongo_url="mongodb://127.0.0.1:27017/wanderlust";
 const Listing = require("./models/listing.js");
 const methodOverride=require("method-override");
+const ejsMate = require("ejs-mate");
 
 app.set("view engine", "ejs");
 app.set("views",path.join(__dirname,"views"));
+app.use(express.static(path.join(__dirname,"public")));
 app.use(express.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
+app.engine('ejs', ejsMate);
 
 //connecting with the db
 main().then(()=>{
@@ -42,7 +46,7 @@ app.get("/listings/:id/edit", async (req, res)=>{
 });
 
 //update route
-app.post("/listings/:id", async (req, res)=>{
+app.put("/listings/:id", async (req, res)=>{
     let {id}=req.params;
     await Listing.findByIdAndUpdate(id, {...req.body.listing});
     res.redirect(`/listings/${id}`);
