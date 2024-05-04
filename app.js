46f40f3440fc672ebb1d211b57a5b2b92f64a04b
Devill0104 <adityamaurya533@gmail.com>
2024-05-04 17:24:12 +0530

    error handling code written, joi for schema validation , new middleware written but error handling not working fine

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const mongo_url="mongodb://127.0.0.1:27017/wanderlust";
 const Listing = require("./models/listing.js");
 const methodOverride=require("method-override");
 const ejsMate = require("ejs-mate");
+const wrapAsync = require("./utils/wrapAsync");
+const ExpressError= require("./utils/ExpressError.js");
+const listingSchema = require("./schema.js");
 
 app.set("view engine", "ejs");
 app.set("views",path.join(__dirname,"views"));
@@ -13,6 +16,8 @@ app.use(express.static(path.join(__dirname,"public")));
 app.use(express.urlencoded({extended:true}));
 app.use(methodOverride("_method"));
 app.engine('ejs', ejsMate);
+
+
 
 //connecting with the db
 main().then(()=>{
@@ -24,10 +29,10 @@ async function main(){
 
 
 //home  route 
-app.get("/listings", async (req, res)=>{
+app.get("/listings",  wrapAsync(async (req, res)=>{
     let allListings = await Listing.find({});
     res.render("listings/index.ejs",{allListings});
-});
+}));
 
 //create route
 app.get("/listings/new", (req,res)=>{
@@ -35,45 +40,62 @@ app.get("/listings/new", (req,res)=>{
 });
 
 //add route
-app.post("/listings", async (req, res)=>{
+app.post("/listings",  wrapAsync(async (req, res)=>{
     let newListing= new Listing(req.body.listing);
     await newListing.save();
     res.redirect("/listings");
-});
+}));
 
 
 //show route
-app.get("/listings/:id", async (req, res)=>{
+app.get("/listings/:id", wrapAsync( async (req, res)=>{
     let {id}=req.params;
-    // console.log(id);
     let listing= await Listing.findById(id);
-    // console.log(listing);
-    // res.send(" working");
     res.render("listings/show.ejs",{listing});
-});
+}));
 
 //edit route
-app.get("/listings/:id/edit", async (req, res)=>{
+app.get("/listings/:id/edit",  wrapAsync(async (req, res)=>{
     let {id}= req.params;
     let listing= await Listing.findById(id);
     res.render("listings/edit.ejs", {listing});
-});
+}));
 
 //update route
-app.put("/listings/:id", async (req, res)=>{
+app.put("/listings/:id",  wrapAsync(async (req, res)=>{
     let {id}=req.params;
     await Listing.findByIdAndUpdate(id, {...req.body.listing});
     res.redirect(`/listings/${id}`);
-});
+}));
 
 //delete route
-app.delete("/listings/:id", async (req, res)=>{
+app.delete("/listings/:id", wrapAsync(async (req, res)=>{
     let {id}=req.params;
     let deletedListing = await Listing.findByIdAndDelete(id);
     console.log(deletedListing);
     res.redirect("/listings");
+}));
+
+//middleware for non-existing routes
+app.all("*", (req, res)=>{
+    throw new ExpressError(404, "Page not found");
 });
 
+//schema validation middleware
+const validateSchema = (req, res, next)=>{
+    let {error} = listingSchema.validate(req.body);
+    if(error){
+        throw new ExpressError(400, error);
+    }else{
+        next();
+    }
+};
+
+// Error handling middleware
+app.use((err, req, res, next)=>{
+    let {statusCode, message}=err;
+    res.status(statusCode).render("error.ejs", {message});
+});
 
 app.listen("8080", ()=>{
     console.log(" server is listening");
