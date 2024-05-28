98e160d2d5f3dad910922b98decae7620e04ec88
Devill0104 <adityamaurya533@gmail.com>
2024-05-28 12:52:46 +0530

    curr usr saved , logic for buttons added, and redirect url aded

--- app.js
+++ app.js
@@ -44,6 +44,8 @@ passport.deserializeUser(User.deserializeUser());
 app.use((req, res,next)=>{
     res.locals.success =  req.flash("success");
     res.locals.error= req.flash("error");
+    //saving the user in a variable
+    res.locals.currUser = req.user;
     next();
 });
 
--- middleware.js
+++ middleware.js
@@ -1,7 +1,16 @@
+//middleware for login authentication
 module.exports.isLoggedIn = (req, res, next)=>{
     if(!req.isAuthenticated()){
+        req.session.redirectUrl = req.originalUrl;   //saving redirect link to reach exact page from where we went to login
         req.flash("error","You must be logged in to create a listing");
         res.redirect("/login");
+    }
+    next();
+}
+
+module.exports.saveRedirectUrl = (req, res, next)=>{
+    if(req.session.redirectUrl){
+        res.locals.redirectUrl = req.session.redirectUrl || "/listings";
     }
     next();
 }
