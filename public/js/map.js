15ae571379dfc3ee471a0814520bfbf89ff8e100
Devill0104 <adityamaurya533@gmail.com>
2024-06-04 19:44:16 +0530

    error in map

--- public/js/map.js
+++ public/js/map.js
@@ -1,4 +1,4 @@
-mapboxgl.accessToken = mapToken;
+mapboxgl.accessToken = "pk.eyJ1IjoiYWRpdHlhMTktMjEiLCJhIjoiY2x3ejVsdnByMDNuODJzc2ZvaHFhangydyJ9._dD8AcvKmw6Ykwyi7W4E_w";
 const map = new mapboxgl.Map({
     container: 'map', // container ID
     center: listing.geometry.coordinates, // starting position [lng, lat]
@@ -7,3 +7,4 @@ const map = new mapboxgl.Map({
 const marker = new mapboxgl.Marker().setLngLat(listing.geometry.coordinates)
                 .setPopup(new mapboxgl.Popup({offset: 25}).setHTML(`<h4>${listing.title}</h4>
                             <p>exact locatin</p>`)).addTo(map);
+console.log(listing);
